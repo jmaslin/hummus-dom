901daf724c4d4c86a17220b7c680d3c733dd3288
Justin Maslin <me@justinmaslin.com>
2018-02-01 19:50:53 -0500

    organize basic examples
    Signed-off-by: Justin Maslin <me@justinmaslin.com>

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,51 @@
+/** @jsx Hummus.chickpea */
+
+import { Hummus } from 'hummus-dom';
+import { merge } from 'immutable';
+
+// simple dom update
+const exampleOne = function exampleOne() {
+  const root = document.getElementById('root');
+
+  const a = (
+    <ul>
+      <li>1</li>
+      <li>3</li>
+      <li>2</li>
+    </ul>
+  )
+
+  const b = (
+    <ul>
+      <li>wow</li>
+      <li>3</li>
+      <li>2</li>
+    </ul>
+  )
+
+  Hummus.addNode(a);
+  setTimeout(() => {
+    Hummus.updateNode(root, b, a);
+  }, 1000)
+};
+
+// input form handling and updating
+const exampleTwo = function exampleTwo() {
+  const root = document.getElementById('root');
+
+  let oldVal = '';
+  const input = document.getElementById('type');
+  input.addEventListener('input', (event) => {
+    const newVal = (
+      <span>{input.value}</span>
+    );
+
+    Hummus.updateNode(root, newVal, oldVal);
+    oldVal = newVal;
+  });
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-  // do your setup here
-  console.log('Initialized app');
+  // exampleOne();
+  exampleTwo();
 });
